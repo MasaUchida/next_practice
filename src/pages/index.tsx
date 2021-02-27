68bc7df8116f77ab42008e0d0f4f2352198a69c4
Masatoshi Uchida <uchidamasa.design@gmail.com>
2021-02-27 15:45:36 +0900

    hoge commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home({ allPostsData }:any ) {
             <title>{siteTitle}</title>
         </Head>
         <section className={utilStyles.headingMd}>
-            <p>Next js 練習中</p>
+            <p>Next js 練習中 ひとまずデプロイした</p>
             <p>
             (This is a sample website - you’ll be building a site like this on{' '}
             <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
